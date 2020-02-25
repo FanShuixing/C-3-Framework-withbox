221f2fdd2640c1af27e7583387b28602d9708b3c
pan <1094170587@qq.com>
2020-02-25 16:02:28 +0800

    fix read img and labels

--- datasets/SHHB/SHHB.py
+++ datasets/SHHB/SHHB.py
@@ -15,12 +15,12 @@ from config import cfg
 
 class SHHB(data.Dataset):
     def __init__(self, data_path, mode, main_transform=None, img_transform=None, gt_transform=None):
-        self.img_path = data_path + '/image'
+        self.img_path = data_path + '/images'
         if mode == 'train':
-            self.gt_path = data_path + '/train_label'
+            self.gt_path = data_path + '/npy_sigma8.0'
             self.gt_txt = data_path + '/train.txt'
         else:
-            self.gt_path = data_path + '/val_label'
+            self.gt_path = data_path + '/npy_sigma8.0'
             self.gt_txt = data_path + '/val.txt'
         with open(self.gt_txt) as fr:
             self.data_files = [re.sub('.csv', '.jpg', filename.strip()) for filename in fr.readlines()]
@@ -45,14 +45,14 @@ class SHHB(data.Dataset):
         return self.num_samples
 
     def read_image_and_gt(self, fname):
-        img = Image.open(os.path.join(self.img_path, fname))
+        img = Image.open(os.path.join(self.img_path, fname+'.jpg'))
         if img.mode == 'L':
             img = img.convert('RGB')
-        img = img.resize((1280, 720))
+        img = img.resize((768, 576))
         # den = sio.loadmat(os.path.join(self.gt_path,os.path.splitext(fname)[0] + '.mat'))
         # den = den['map']
         # den = pd.read_csv(os.path.join(self.gt_path, os.path.splitext(fname)[0] + '.csv'), sep=',', header=None).values
-        den = np.load(os.path.join(self.gt_path, os.path.splitext(fname)[0] + '.npy'))
+        den = np.load(os.path.join(self.gt_path, fname + '.npy'))
 
         den = den.astype(np.float32, copy=False)
         den = Image.fromarray(den)
