5af777840af2b632e8382c78cf34c2b82803e07c
pan <1094170587@qq.com>
2020-03-19 17:05:25 +0800

    增加修的错误率计算方式

--- gener_wrong.py
+++ gener_wrong.py
@@ -22,6 +22,15 @@ def main():
     w_idx_than_1 = np.where(abs(info[:, 1] - info[:, 2]) > 1)[0]
     print('错误超过1的数目/总数=%s/%s' % (len(w_idx_than_1), info.shape[0]), '=', len(w_idx_than_1) / info.shape[0])
 
+    idx = info[:, 1] - info[:, 2]
+    err_more_idx = np.where(idx > 0)[0]
+    a = (info[err_more_idx][:, 1] - info[err_more_idx][:, 2]) / (info[err_more_idx][:, 2])
+    print("根数差值大于0的错误率统计:", np.sum(a) / len(a))
+
+    err_less_idx = np.where(idx < 0)[0]
+    a = abs(info[err_less_idx][:, 1] - info[err_less_idx][:, 2]) / (info[err_less_idx][:, 2])
+    print("根数差值小于0的错误率统计:", np.sum(a) / len(a))
+
 
 if __name__ == '__main__':
     main()
