bff1ebe5f9187f67148a6ebf8d468d14fecc0381
pan <1094170587@qq.com>
2020-04-27 10:49:52 +0800

    修改默认配置

--- compute_f1score.sh
+++ compute_f1score.sh
@@ -1,13 +1,12 @@
 
 # v16
-for type in normal slope occlude;
+for type in normal;
 do
-    python pascalvoc.py  --gtfolder /input1/normal --detfolder /output/result/result_16_$type --threshold 0.7
+    python pascalvoc.py  --gtfolder /input0 --detfolder /output/result/result_16_$type --threshold 0.7
 done
 
 # v14
-for type in normal slope occlude;
+for type in normal;
 do
-    python pascalvoc.py  --gtfolder /input0/normal --detfolder /output/result/result_14_$type --threshold 0.7
+    python pascalvoc.py  --gtfolder /input0 --detfolder /output/result/result_14_$type --threshold 0.7
 done
-
