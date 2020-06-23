349492a0525437bb57875903b50403d73ca3fed6
root <1094170587@qq.com>
2020-06-23 09:08:46 +0000

    mucai2超参数调参

--- gener_wrong.py
+++ gener_wrong.py
@@ -17,33 +17,32 @@ def main(args):
 
     diff = abs(info[:, 2] - info[:, 3])
     idx_list = np.where(diff > 0)
-    for idx in idx_list[0]:
-        name = info[idx][0].split('/')[-1].replace('.jpg', '')
-        ori = os.path.join(args.root_dir, args.meta_file,
-                           'images/%s_pred_%s_gt_%s_box_%s.png' % (name, info[idx][1], info[idx][2], info[idx][3]))
-        target = os.path.join(args.output_dir, args.meta_file,
-                              '%s_pred%s_gt%s_box_%s.png' % (name, info[idx][1], info[idx][2], info[idx][3]))
-        shutil.copy(ori, target)
+    with open('result_report.txt','a+') as fr:
+        for idx in idx_list[0]:
+            name = info[idx][0].split('/')[-1].replace('.jpg', '')
+            ori = os.path.join(args.root_dir, args.meta_file,
+                               'images/%s_pred_%s_gt_%s_box_%s.png' % (name, info[idx][1], info[idx][2], info[idx][3]))
+            target = os.path.join(args.output_dir, args.meta_file,
+                                  '%s_pred%s_gt%s_box_%s.png' % (name, info[idx][1], info[idx][2], info[idx][3]))
+            shutil.copy(ori, target)
 
-    w_idx = np.where(diff > 0)[0]
-    print('*' * 10, args.meta_file, '*' * 10)
-    print('图片数目:错误数/总数=%s/%s' % (len(w_idx), info.shape[0]), '=', len(w_idx) / info.shape[0])
+        w_idx = np.where(diff > 0)[0]
+        fr.write('*' * 10+args.meta_file+'*' * 10)
+        fr.write('\n\n')
+        fr.write('图片数目:错误数/总数=%s/%s' % (len(w_idx), info.shape[0])+ '='+ str(len(w_idx) / info.shape[0]))
+        fr.write('\n\n')
+#         print('*' * 10, args.meta_file, '*' * 10)
+#         print('图片数目:错误数/总数=%s/%s' % (len(w_idx), info.shape[0]), '=', len(w_idx) / info.shape[0])
 
-    w_idx_than_1 = np.where(diff > 1)[0]
-    print('图片数目:错误超过1的数目/总数=%s/%s' % (len(w_idx_than_1), info.shape[0]), '=', len(w_idx_than_1) / info.shape[0])
+        w_idx_than_1 = np.where(diff > 1)[0]
+        fr.write('图片数目:错误超过1的数目/总数=%s/%s' % (len(w_idx_than_1), info.shape[0])+\
+                 '='+str(len(w_idx_than_1) / info.shape[0]))
+        fr.write('\n\n\n')
+#         print('图片数目:错误超过1的数目/总数=%s/%s' % (len(w_idx_than_1), info.shape[0]), '=', len(w_idx_than_1) / info.shape[0])
 
 
-#     idx = info[:, 2] - info[:, 3]
-#     err_more_idx = np.where(idx > 0)[0]
-#     a = (info[err_more_idx][:, 1] - info[err_more_idx][:, 2]) / (info[err_more_idx][:, 2])
-#     if len(a) == 0:
-#         print("木材根数:根数差值大于0的错误率统计:", 0)
-#     else:
-#         print("木材根数:根数差值大于0的错误率统计:", np.sum(a) / len(a))
 
-#     err_less_idx = np.where(idx < 0)[0]
-#     a = abs(info[err_less_idx][:, 1] - info[err_less_idx][:, 2]) / (info[err_less_idx][:, 2])
-#     print("木材根数:根数差值小于0的错误率统计:", np.sum(a) / len(a))
+
 
 
 if __name__ == '__main__':
