8f047b54b1c903cea5526f2c20841c29598df3f1
pan <1094170587@qq.com>
2020-07-01 15:04:46 +0800

    增加数据增强

--- datasets/SHHB/loading_data.py
+++ datasets/SHHB/loading_data.py
@@ -2,7 +2,7 @@ import torchvision.transforms as standard_transforms
 from torch.utils.data import DataLoader
 import misc.transforms as own_transforms
 from .SHHB import SHHB
-from .setting import cfg_data 
+from .setting import cfg_data
 import torch
 
 
@@ -10,13 +10,15 @@ def loading_data():
     mean_std = cfg_data.MEAN_STD
     log_para = cfg_data.LOG_PARA
     train_main_transform = own_transforms.Compose([
-    	#own_transforms.RandomCrop(cfg_data.TRAIN_SIZE),
-    	own_transforms.RandomHorizontallyFlip()
+        own_transforms.RandomColorJitter(),
+        own_transforms.RandomCrop(cfg_data.TRAIN_SIZE),
+        own_transforms.RandomHorizontallyFlip(),
+        own_transforms.RandomVerticallyFlip(),
     ])
     val_main_transform = own_transforms.Compose([
         own_transforms.RandomCrop(cfg_data.TRAIN_SIZE)
     ])
-    val_main_transform = None
+    #     val_main_transform = None
     img_transform = standard_transforms.Compose([
         standard_transforms.ToTensor(),
         standard_transforms.Normalize(*mean_std)
@@ -29,11 +31,13 @@ def loading_data():
         standard_transforms.ToPILImage()
     ])
 
-    train_set = SHHB(cfg_data.DATA_PATH, 'train',main_transform=train_main_transform, img_transform=img_transform, gt_transform=gt_transform)
-    train_loader = DataLoader(train_set, batch_size=cfg_data.TRAIN_BATCH_SIZE, num_workers=8, shuffle=True, drop_last=True)
-    
+    train_set = SHHB(cfg_data.DATA_PATH, 'train', main_transform=train_main_transform, img_transform=img_transform,
+                     gt_transform=gt_transform)
+    train_loader = DataLoader(train_set, batch_size=cfg_data.TRAIN_BATCH_SIZE, num_workers=8, shuffle=True,
+                              drop_last=True)
 
-    val_set = SHHB(cfg_data.DATA_PATH, 'test', main_transform=val_main_transform, img_transform=img_transform, gt_transform=gt_transform)
+    val_set = SHHB(cfg_data.DATA_PATH, 'test', main_transform=val_main_transform, img_transform=img_transform,
+                   gt_transform=gt_transform)
     val_loader = DataLoader(val_set, batch_size=cfg_data.VAL_BATCH_SIZE, num_workers=8, shuffle=True, drop_last=False)
 
     return train_loader, val_loader, restore_transform
--- misc/transforms.py
+++ misc/transforms.py
@@ -4,28 +4,68 @@ import numpy as np
 from PIL import Image, ImageOps, ImageFilter
 from config import cfg
 import torch
+from torchvision import transforms
+
+
 # ===============================img tranforms============================
 
 class Compose(object):
     def __init__(self, transforms):
         self.transforms = transforms
 
-    def __call__(self, img, den,hm_mask, bboxes):
+    def __call__(self, img, den, hm_mask, bboxes):
         for t in self.transforms:
-            img,den, hm_mask,bboxes = t(img,den, hm_mask,bboxes)
-        return img, den,hm_mask,bboxes
+            img, den, hm_mask, bboxes = t(img, den, hm_mask, bboxes)
+        return img, den, hm_mask, bboxes
+
+
+class RandomColorJitter(object):
+    def __call__(self, img, den, hm_mask, bboxes):
+        if random.random() < 0.5:
+            img = transforms.ColorJitter(brightness=random.uniform(0, 2), contrast=random.uniform(0, 2))(img)
+
+        return img, den, hm_mask, bboxes
+
+
+class RandomVerticallyFlip(object):
+    def __call__(self, img, den, hm_mask, bboxes):
+        if random.random() < 0.5:
+            w, h = img.size
+            box_h = bboxes[:, 3] - bboxes[:, 1]
+            bboxes[:, 1] = h - bboxes[:, 1] - box_h
+            bboxes[:, 3] = bboxes[:, 1] + box_h
+            img, den, hm_mask = img.transpose(Image.FLIP_TOP_BOTTOM), den.transpose(
+                Image.FLIP_TOP_BOTTOM), hm_mask.transpose(Image.FLIP_TOP_BOTTOM)
+        #         visual_debug(img,bboxes,den,'final')
+
+        return img, den, hm_mask, bboxes
+
+
+def visual_debug(img, bboxes, den, name):
+    # 框也要对应的发生改变
+
+    img_a = np.array(img)
+    import cv2
+    for each in bboxes:
+        x0, y0, x1, y1 = each
+        cv2.rectangle(img_a, (int(x0), int(y0)), (int(x1), int(y1)), (0, 0, 255), 2)
+    import random
+    name_seed = random.random()
+    cv2.imwrite('%s/%s.jpg' % (name, name_seed), img_a)
+    np.save('%s/%s.npy' % (name, name_seed), den)
+
 
 class RandomHorizontallyFlip(object):
-    def __call__(self, img,den, hm_mask, bboxes):
+    def __call__(self, img, den, hm_mask, bboxes):
         if random.random() < 0.5:
-
             w, h = img.size
             box_w = bboxes[:, 2] - bboxes[:, 0]
             bboxes[:, 0] = w - bboxes[:, 0] - box_w
             bboxes[:, 2] = bboxes[:, 0] + box_w
-            return img.transpose(Image.FLIP_LEFT_RIGHT),\
-        den.transpose(Image.FLIP_LEFT_RIGHT),hm_mask.transpose(Image.FLIP_LEFT_RIGHT), bboxes
-        return img, den,hm_mask, bboxes
+            return img.transpose(Image.FLIP_LEFT_RIGHT), \
+                   den.transpose(Image.FLIP_LEFT_RIGHT), hm_mask.transpose(Image.FLIP_LEFT_RIGHT), bboxes
+        return img, den, hm_mask, bboxes
+
 
 class RandomCrop(object):
     def __init__(self, size, padding=0):
@@ -35,7 +75,7 @@ class RandomCrop(object):
             self.size = size
         self.padding = padding
 
-    def __call__(self, img, mask, dst_size=None):
+    def __call__(self, img, mask, hm_mask, bboxes, dst_size=None):
         if self.padding > 0:
             img = ImageOps.expand(img, border=self.padding, fill=0)
             mask = ImageOps.expand(mask, border=self.padding, fill=0)
@@ -51,9 +91,44 @@ class RandomCrop(object):
         if w < tw or h < th:
             return img.resize((tw, th), Image.BILINEAR), mask.resize((tw, th), Image.NEAREST)
 
-        x1 = random.randint(0, w - tw)
-        y1 = random.randint(0, h - th)
-        return img.crop((x1, y1, x1 + tw, y1 + th)), mask.crop((x1, y1, x1 + tw, y1 + th))
+        if random.random() < 0.5:
+
+            x1 = random.randint(0, w - tw)
+            y1 = random.randint(0, h - th)
+            # visual_debug(img,bboxes,den,'a')
+            bboxes[:, 0] -= x1
+            bboxes[:, 1] -= y1
+            bboxes[:, 2] -= x1
+            bboxes[:, 3] -= y1
+
+            # 删除x0,y0<0的框
+            remove_ixs = np.where(bboxes[:, 0] < 0)[0]
+            bboxes = np.delete(bboxes, remove_ixs, axis=0)
+            remove_ixs = np.where(bboxes[:, 1] < 0)[0]
+            bboxes = np.delete(bboxes, remove_ixs, axis=0)
+
+            # 删除x1>tw的
+            remove_ixs = np.where(bboxes[:, 2] > tw)[0]
+            bboxes = np.delete(bboxes, remove_ixs, axis=0)
+            # 删除y1>th的
+            remove_ixs = np.where(bboxes[:, 3] > th)[0]
+            bboxes = np.delete(bboxes, remove_ixs, axis=0)
+
+            img = img.crop((x1, y1, x1 + tw, y1 + th))
+            den = mask.crop((x1, y1, x1 + tw, y1 + th))
+            hm_mask = hm_mask.crop((x1, y1, x1 + tw, y1 + th))
+        else:
+            img, den, hm_mask = img.resize((tw, th), Image.BILINEAR), mask.resize((tw, th),
+                                                                                  Image.NEAREST), hm_mask.resize(
+                (tw, th), Image.NEAREST)
+            bboxes[:, 0] = bboxes[:, 0] / w * tw
+            bboxes[:, 1] = bboxes[:, 1] / h * th
+            bboxes[:, 2] = bboxes[:, 2] / w * tw
+            bboxes[:, 3] = bboxes[:, 3] / h * th
+
+        # debug
+        #         visual_debug(img,bboxes,den,'crop')
+        return img, den, hm_mask, bboxes
 
 
 class CenterCrop(object):
@@ -71,13 +146,13 @@ class CenterCrop(object):
         return img.crop((x1, y1, x1 + tw, y1 + th)), mask.crop((x1, y1, x1 + tw, y1 + th))
 
 
-
 class FreeScale(object):
     def __init__(self, size):
         self.size = size  # (h, w)
 
     def __call__(self, img, mask):
-        return img.resize((self.size[1], self.size[0]), Image.BILINEAR), mask.resize((self.size[1], self.size[0]), Image.NEAREST)
+        return img.resize((self.size[1], self.size[0]), Image.BILINEAR), mask.resize((self.size[1], self.size[0]),
+                                                                                     Image.NEAREST)
 
 
 class ScaleDown(object):
@@ -85,7 +160,7 @@ class ScaleDown(object):
         self.size = size  # (h, w)
 
     def __call__(self, mask):
-        return  mask.resize((self.size[1]/cfg.TRAIN.DOWNRATE, self.size[0]/cfg.TRAIN.DOWNRATE), Image.NEAREST)
+        return mask.resize((self.size[1] / cfg.TRAIN.DOWNRATE, self.size[0] / cfg.TRAIN.DOWNRATE), Image.NEAREST)
 
 
 class Scale(object):
@@ -94,8 +169,8 @@ class Scale(object):
 
     def __call__(self, img, mask):
         if img.size != mask.size:
-            print( img.size )
-            print( mask.size )          
+            print(img.size)
+            print(mask.size)
         assert img.size == mask.size
         w, h = img.size
         if (w <= h and w == self.size) or (h <= w and h == self.size):
@@ -135,8 +210,9 @@ class LabelNormalize(object):
     def __call__(self, tensor):
         # tensor = 1./(tensor+self.para).log()
         tensor = torch.from_numpy(np.array(tensor))
-        tensor = tensor*self.para
+        tensor = tensor * self.para
         return tensor
+
 
 class GTScaleDown(object):
     def __init__(self, factor=8):
@@ -144,8 +220,8 @@ class GTScaleDown(object):
 
     def __call__(self, img):
         w, h = img.size
-        if self.factor==1:
+        if self.factor == 1:
             return img
-        tmp = np.array(img.resize((w//self.factor, h//self.factor), Image.BICUBIC))*self.factor*self.factor
+        tmp = np.array(img.resize((w // self.factor, h // self.factor), Image.BICUBIC)) * self.factor * self.factor
         img = Image.fromarray(tmp)
         return img
